43e3294b7d30aba08a4a413f513203bd5b0360ec
Brendan Tan <brendantan@outlook.com>
2022-05-27 17:18:28 +0800

    Added basic Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/cupertino.dart';
 import 'listings.dart';
 import 'login.dart';
 import 'create.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options:DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
