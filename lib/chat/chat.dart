90e5ae5ac7450caa79c4d12343d467a8a88a68ac
Brendan Tan <brendantan@outlook.com>
2022-07-20 23:44:31 +0800

    Set date time and location of meetup

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -4,9 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:pull_down_button/pull_down_button.dart';
 
 import '../listings/listing.dart';
 import '../listings/report.dart';
+import '../locations/location.dart';
 import 'chathelper.dart';
 import 'message.dart';
 import 'bubble.dart';
@@ -44,12 +46,19 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     final helper = ModalRoute.of(context)?.settings.arguments as ChatHelper;
-    final String buyerId = helper.buyerId;
     final Listing listing = helper.listing;
+    final String buyerId = helper.buyerId;
+
+    int meetLocation = listing.location;
+    bool locationSetStateCalled = false;
+    DateTime? meetTime;
+    bool timeSetStateCalled = false;
+
     final String price = listing.price == 0
         ? 'Free'
         : NumberFormat.currency(locale: 'en_SG', symbol: '\$')
             .format(listing.price);
+    final int location = listing.location;
 
     final CollectionReference chat = FirebaseFirestore.instance
         .collection('search_listings')
@@ -119,7 +128,6 @@ class _ChatState extends State<Chat> {
                                 } else {
                                   newDate = false;
                                 }
-
                                 Container dateStamp = Container(
                                     margin: const EdgeInsets.fromLTRB(0, 14, 0, 5),
                                     child: Text(dates[index],
@@ -145,7 +153,7 @@ class _ChatState extends State<Chat> {
                                   // first chat bubble at the top, always show date stamp
                                   return Column(
                                     children: [
-                                      const SizedBox(height: 72 - 14),
+                                      const SizedBox(height: 72 + 32 - 14),
                                       dateStamp,
                                       Bubble(msg: msg, isMine: isMine)
                                     ],
@@ -176,44 +184,192 @@ class _ChatState extends State<Chat> {
                         })),
                 Align(
                     alignment: Alignment.topCenter,
-                    child: ClipRect(
-                      child: BackdropFilter(
-                          filter: ui.ImageFilter.blur(sigmaX: 7.5, sigmaY: 7.5),
-                          child: Container(
-                              height: 72,
-                              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                              decoration: const BoxDecoration(
-                                  color: Color(0xEBF2F2F7),
-                                  border: Border(
-                                      bottom: BorderSide(
-                                          width: 0.3,
-                                          color: CupertinoColors
-                                              .opaqueSeparator))),
-                              child: Row(
-                                mainAxisAlignment:
+                    child: Column(
+                      children: [
+                        ClipRect(
+                          child: BackdropFilter(
+                              filter: ui.ImageFilter.blur(sigmaX: 7.5, sigmaY: 7.5),
+                              child: Container(
+                                  height: 72,
+                                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                  decoration: const BoxDecoration(
+                                      color: Color(0xEBF2F2F7),
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 0.3,
+                                              color: CupertinoColors
+                                                  .opaqueSeparator))),
+                                  child: Row(
+                                    mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
+                                    children: [
+                                      Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment:
                                         CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(listing.title,
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w600)),
-                                      const SizedBox(height: 7),
-                                      Text(price,
-                                          style: const TextStyle(fontSize: 16))
+                                        children: <Widget>[
+                                          Text(listing.title,
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.w600),
+                                              textScaleFactor: 1.14,
+                                          ),
+                                          const SizedBox(height: 8),
+                                          Wrap(
+                                            direction: Axis.horizontal,
+                                            crossAxisAlignment: WrapCrossAlignment.center,
+                                            children: [
+                                              const Icon(CupertinoIcons.money_dollar_circle_fill, size: 20, color: Color(0xFF86888C)),
+                                              const SizedBox(width: 6),
+                                              Text(price,
+                                                  textScaleFactor: 0.90),
+                                              const SizedBox(width: 30),
+                                              const Icon(CupertinoIcons.house_fill, size: 20, color: Color(0xFF86888C)),
+                                              const SizedBox(width: 6),
+                                              Text(Location.values[location].locationName,
+                                                  textScaleFactor: 0.90)
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      Container(
+                                          padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
+                                          child: listing.showImage(square: true, ind: 0)
+                                      )
                                     ],
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
-                                    child: listing.showImage(square: true, ind: 0)
-                                  )
-                                ],
-                              ))),
-                    )),
+                                  ))),
+                        ),
+                        ClipRect(
+                          child: BackdropFilter(
+                              filter: ui.ImageFilter.blur(sigmaX: 7.5, sigmaY: 7.5),
+                              child: Container(
+                                  height: 32,
+                                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                  decoration: const BoxDecoration(
+                                      color: Color(0xEBF6F6FB),
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 0.3,
+                                              color: CupertinoColors
+                                                  .opaqueSeparator))),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Wrap(
+                                        crossAxisAlignment: WrapCrossAlignment.center,
+                                        spacing: 6,
+                                        children: [
+                                          const Icon(CupertinoIcons.location, size: 16, color: CupertinoColors.secondaryLabel),
+                                          StatefulBuilder(
+                                            builder: (context, innerSetState) {
+                                              helper.getMeetLocation().then((loc) {
+                                                if (mounted && !locationSetStateCalled) {
+                                                  innerSetState(() {
+                                                    meetLocation = loc;
+                                                    locationSetStateCalled = true;
+                                                  });
+                                                }
+                                              });
+                                              return PullDownButton(
+                                                  position: PullDownMenuPosition.under,
+                                                  itemBuilder: (context) {
+                                                    return Location.values
+                                                        .map((loc) => PullDownMenuItem(
+                                                        title: loc.locationName,
+                                                        textStyle: TextStyle(
+                                                          color: CupertinoColors.black,
+                                                          fontFamily: CupertinoTheme.of(context).textTheme.textStyle.fontFamily,
+                                                        ),
+                                                        onTap: () {
+                                                          meetLocation = loc.index;
+                                                          helper.manageMeetLocation(meetLocation);
+                                                        })
+                                                    ).toList();
+                                                  },
+                                                  buttonBuilder: (context, showMenu) {
+                                                    return Container(
+                                                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(6), color: CupertinoColors.tertiarySystemFill),
+                                                      height: 22,
+                                                      child: CupertinoButton(
+                                                          padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                                                          onPressed: showMenu,
+                                                          child: Text(Location.values[meetLocation].locationName, style: const TextStyle(fontSize: 14.5),)
+                                                      ),
+                                                    );
+                                                  }
+                                              );
+                                            }
+                                          )
+                                        ],
+                                      ),
+                                      Wrap(
+                                        crossAxisAlignment: WrapCrossAlignment.center,
+                                        spacing: 6,
+                                        children: [
+                                          StatefulBuilder(
+                                            builder: (context, innerSetState) {
+                                              helper.getMeetTime().then((dt) {
+                                                if (mounted && !timeSetStateCalled) {
+                                                  innerSetState(() {
+                                                    meetTime = dt;
+                                                    timeSetStateCalled = true;
+                                                  });
+                                                }
+                                              });
+
+                                              return Container(
+                                                decoration: BoxDecoration(borderRadius: BorderRadius.circular(6), color: CupertinoColors.tertiarySystemFill),
+                                                height: 22,
+                                                child: CupertinoButton(
+                                                    padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                                                    onPressed: () async {
+                                                      DateTime? dt = await showCupertinoModalPopup<DateTime>(
+                                                          context: context,
+                                                          builder: (BuildContext context) => WillPopScope(
+                                                              onWillPop: () async {
+                                                                Navigator.pop(context, meetTime);
+                                                                return true;
+                                                              },
+                                                              child: Container(
+                                                                height: 216,
+                                                                padding: const EdgeInsets.only(top: 6.0),
+                                                                margin: EdgeInsets.only(
+                                                                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                                                                ),
+                                                                color: CupertinoColors.systemBackground.resolveFrom(context),
+                                                                child: SafeArea(
+                                                                  top: false,
+                                                                  child: CupertinoDatePicker(
+                                                                    initialDateTime: meetTime,
+                                                                    use24hFormat: false,
+                                                                    onDateTimeChanged: (DateTime newTime) {
+                                                                      innerSetState(() {
+                                                                        meetTime = newTime;
+                                                                      });
+                                                                    },
+                                                                  ),
+                                                                ),
+                                                              )
+                                                          )
+                                                      );
+                                                      if (dt != null) {
+                                                        helper.manageMeetTime(dt);
+                                                      }
+                                                    },
+                                                    child: Text(meetTime == null ? 'Not set' : DateFormat('hh:mm a, dd MMM').format(meetTime!), style: const TextStyle(fontSize: 14.5))
+                                                )
+                                              );
+                                            },
+                                          ),
+                                          const Icon(CupertinoIcons.time, size: 16, color: CupertinoColors.secondaryLabel)
+                                        ],
+                                      )
+                                    ],
+                                  ))),
+                        )
+                      ],
+                    )
+                ),
                 Align(
                     alignment: Alignment.bottomCenter,
                     child: ClipRect(
@@ -260,9 +416,7 @@ class _ChatState extends State<Chat> {
                                                                     .text
                                                                     .trim(),
                                                             time: '');
-                                                        ChatHelper.manageChat(
-                                                            listing.docId,
-                                                            buyerId);
+                                                        helper.manageChat();
                                                         chat.add(
                                                             msg.toFirestore());
                                                         messageController
--- lib/chat/chathelper.dart
+++ lib/chat/chathelper.dart
@@ -5,22 +5,95 @@ import '../listings/listing.dart';
 class ChatHelper {
   final Listing listing;
   final String buyerId;
+  late int meetLocation;
+  DateTime? meetTime;
 
-  ChatHelper(this.listing, this.buyerId);
+  ChatHelper(this.listing, this.buyerId) {
+    meetLocation = listing.location;
+  }
 
-  static void manageChat(String listingId, String buyerId) {
+  Future<DateTime?> getMeetTime() async {
+    DocumentSnapshot<Map<String, dynamic>> data = await FirebaseFirestore.instance
+        .collection('search_listings')
+        .doc(listing.docId)
+        .collection('chats')
+        .doc(buyerId).get();
+
+    try {
+      meetTime = (data['meetTime'] as Timestamp).toDate();
+    } catch (error) {
+      meetTime = null;
+    }
+
+    return meetTime;
+  }
+
+  Future<int> getMeetLocation() async {
+    DocumentSnapshot<Map<String, dynamic>> data = await FirebaseFirestore.instance
+        .collection('search_listings')
+        .doc(listing.docId)
+        .collection('chats')
+        .doc(buyerId).get();
+
+    try {
+      int receivedLocation = data['meetLocation'];
+      if (meetLocation != receivedLocation) {
+        meetLocation = receivedLocation;
+      }
+    } catch (error) {
+      meetLocation = listing.location;
+    }
+
+    return meetLocation;
+  }
+
+  void manageChat() {
     final DocumentReference chat = FirebaseFirestore.instance
         .collection('search_listings')
-        .doc(listingId)
+        .doc(listing.docId)
         .collection('chats')
         .doc(buyerId);
 
     chat.get().then((docSnapshot) {
-    if (docSnapshot.exists) {
-      chat.update({'lastUpdated': Timestamp.fromDate(DateTime.now())});
-    } else {
-      chat.set({'lastUpdated': Timestamp.fromDate(DateTime.now())});
-    }
+      if (docSnapshot.exists) {
+        chat.update({'lastUpdated': Timestamp.fromDate(DateTime.now())});
+      } else {
+        chat.set({'lastUpdated': Timestamp.fromDate(DateTime.now())});
+      }
+    });
+  }
+
+  void manageMeetLocation(int location) {
+    final DocumentReference chat = FirebaseFirestore.instance
+        .collection('search_listings')
+        .doc(listing.docId)
+        .collection('chats')
+        .doc(buyerId);
+
+    chat.get().then((docSnapshot) {
+      meetLocation = location;
+      if (docSnapshot.exists) {
+        chat.update({'meetLocation': location});
+      } else {
+        chat.set({'meetLocation': location});
+      }
+    });
+  }
+
+  void manageMeetTime(DateTime dateTime) {
+    final DocumentReference chat = FirebaseFirestore.instance
+        .collection('search_listings')
+        .doc(listing.docId)
+        .collection('chats')
+        .doc(buyerId);
+
+    chat.get().then((docSnapshot) {
+      meetTime = dateTime;
+      if (docSnapshot.exists) {
+        chat.update({'meetTime': Timestamp.fromDate(dateTime)});
+      } else {
+        chat.set({'meetTime': Timestamp.fromDate(dateTime)});
+      }
     });
   }
 }
